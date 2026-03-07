449cdb5d4a627c02233a5cc418a8517403f3c36a
joozery <zerryboy28@gmail.com>
2026-03-07 17:56:04 +0700

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
     host: true
@@ -12,4 +18,4 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets'
   }
-}) 
+})
